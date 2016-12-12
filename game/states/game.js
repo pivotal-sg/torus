f4e4106777835cc7b4ef50a190eaabb126ad8a6d
Yifeng Hou + Benjamin Tan <pair+yh+bt@pivotal.io>
2016-12-12 10:56:22 +0800

    publish: [Finishes #135763325] Obstacles can move in any direction
    generated from commit 0f59dae8de3cca7a2498cf2be0b0968e13f54883

--- game/states/game.js
+++ game/states/game.js
@@ -5,6 +5,8 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var player_1 = require('../prefabs/player');
+var GRAVITY = 100;
+var NUM_OF_OBSTACLES = 10;
 var Game = (function (_super) {
     __extends(Game, _super);
     function Game() {
@@ -13,18 +15,21 @@ var Game = (function (_super) {
     Game.prototype.create = function () {
         this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'outerSpace');
         this.player = new player_1.Player(this.game, this.world.centerX, this.world.centerY);
-        var numOfCircles = this.generateRandom(100);
+        var numOfCircles = this.generateRandom(NUM_OF_OBSTACLES);
         this.obstacles = this.game.add.group();
         this.obstacles.enableBody = true;
         for (var i = 0; i < numOfCircles; i++) {
-            this.obstacles.create(this.generateRandom(this.world.width), this.generateRandom(this.world.height), 'circle');
+            var obstacle = this.obstacles.create(this.generateRandom(this.world.width), this.generateRandom(this.world.height), 'circle');
+            obstacle.body.gravity.x = this.generateRandom(GRAVITY, true);
+            obstacle.body.gravity.y = this.generateRandom(GRAVITY, true);
         }
     };
     Game.prototype.update = function () {
         this.game.physics.arcade.collide(this.player, this.obstacles, this.collide, null, this);
     };
-    Game.prototype.generateRandom = function (number) {
-        return Math.ceil(Math.random() * number + 1);
+    Game.prototype.generateRandom = function (number, allowNegative) {
+        if (allowNegative === void 0) { allowNegative = false; }
+        return Math.ceil((Math.random() - (allowNegative ? 0.5 : 0)) * number + 1);
     };
     Game.prototype.collide = function (player) {
         player.kill();
