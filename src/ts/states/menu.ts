b23e90199dcb56a0a2479ca4c6cde4b66e686f45
Yifeng Hou + Benjamin Tan <pair+yh+bt@pivotal.io>
2016-12-12 13:59:54 +0800

    [#Finishes #135762571] Clicking the start button OR pressing [spacebar] starts the game.

--- src/ts/states/menu.ts
+++ src/ts/states/menu.ts
@@ -1,30 +1,39 @@
 export class Menu extends Phaser.State {
+  private spaceKey;
 
   create() {
 
     let titleFontStyle = {
       font: '180px VT323',
-      fill: '#83769C'
+      fill: '#8C5C9E'
+    };
+
+    let subTitleFontStyle = {
+      font: '30px VT323',
+      fill: '#FFB83B'
     };
 
     let titleText = this.add.text(this.world.centerX, 100, 'TORUS', titleFontStyle);
     titleText.anchor.setTo(0.5, 0.5);
 
-    let buttonFontStyle = {
-      font: '30px VT323',
-      fill: '#00E232'
-    };
-
     let spaceman = this.game.add.image(this.world.centerX, this.world.centerY, 'spaceman');
     spaceman.anchor.setTo(0.5, 0.5);
 
-    let gameButton = this.add.text(this.world.centerX, this.world.height - 150, 'PLAY!', buttonFontStyle);
-    gameButton.anchor.setTo(0.5, 0.5);
-    gameButton.inputEnabled = true;
-    gameButton.events.onInputDown.addOnce(this.buttonClicked, this);
+    let startButton = this.add.text(this.world.centerX, this.world.height - 90, 'PRESS HERE OR SPACE TO START', subTitleFontStyle);
+    startButton.anchor.setTo(0.5, 0.5);
+    startButton.inputEnabled = true;
+    startButton.events.onInputDown.addOnce(this.buttonClicked, this);
+
+    this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   }
 
   buttonClicked() {
     this.game.state.start('Game');
+  }
+
+  update() {
+    if (this.spaceKey.isDown) {
+      this.game.state.start('Game');
+    }
   }
 }
