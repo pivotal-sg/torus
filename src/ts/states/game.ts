0f59dae8de3cca7a2498cf2be0b0968e13f54883
Yifeng Hou + Benjamin Tan <pair+yh+bt@pivotal.io>
2016-12-12 10:55:37 +0800

    [Finishes #135763325] Obstacles can move in any direction

--- src/ts/states/game.ts
+++ src/ts/states/game.ts
@@ -1,5 +1,8 @@
-import { Player } from '../prefabs/player';
+import {Player} from '../prefabs/player';
 import Group = Phaser.Group;
+
+const GRAVITY = 100;
+const NUM_OF_OBSTACLES = 10;
 
 export class Game extends Phaser.State {
 
@@ -10,13 +13,15 @@ export class Game extends Phaser.State {
     this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'outerSpace');
 
     this.player = new Player(this.game, this.world.centerX, this.world.centerY);
-    let numOfCircles = this.generateRandom(100);
+    let numOfCircles = this.generateRandom(NUM_OF_OBSTACLES);
 
     this.obstacles = this.game.add.group();
     this.obstacles.enableBody = true;
 
     for (let i = 0; i < numOfCircles; i++) {
-      this.obstacles.create(this.generateRandom(this.world.width), this.generateRandom(this.world.height), 'circle')
+      let obstacle = this.obstacles.create(this.generateRandom(this.world.width), this.generateRandom(this.world.height), 'circle')
+      obstacle.body.gravity.x = this.generateRandom(GRAVITY, true);
+      obstacle.body.gravity.y = this.generateRandom(GRAVITY, true);
     }
   }
 
@@ -24,11 +29,11 @@ export class Game extends Phaser.State {
     this.game.physics.arcade.collide(this.player, this.obstacles, this.collide, null, this);
   }
 
-  private generateRandom(number: number) {
-    return Math.ceil(Math.random() * number + 1);
+  private generateRandom(number: number, allowNegative = false) {
+    return Math.ceil((Math.random() - (allowNegative ? 0.5 : 0)) * number + 1);
   }
 
-  private collide(player : Player) {
+  private collide(player: Player) {
     player.kill();
     this.game.state.start('Game');
   }
