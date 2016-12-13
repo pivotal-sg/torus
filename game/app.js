efd3d74c5965f656e5ec9c037051e5756df34efb
Pair <pair@pivotal.io>
2016-12-13 17:37:26 +0800

    publish: Fix gulpfile
    generated from commit b35ebc83d433b0b828527e879225ca66eb5f68d4

--- game/app.js
+++ game/app.js
@@ -8,6 +8,7 @@ var boot_1 = require('./states/boot');
 var loading_1 = require('./states/loading');
 var menu_1 = require('./states/menu');
 var game_1 = require('./states/game');
+var score_1 = require("./states/score");
 var App = (function (_super) {
     __extends(App, _super);
     function App() {
@@ -16,6 +17,7 @@ var App = (function (_super) {
         this.state.add('Loading', loading_1.Loading);
         this.state.add('Menu', menu_1.Menu);
         this.state.add('Game', game_1.Game);
+        this.state.add('Score', score_1.Score);
         this.state.start('Boot');
     }
     return App;
--- game/states/game.js
+++ game/states/game.js
@@ -9,8 +9,7 @@ var random_generator_1 = require('../helpers/random_generator');
 var formatter_1 = require("../helpers/formatter");
 var VELOCITY = 100;
 var INITIAL_OBSTACLE_SPEED = 100;
-var NUM_OF_OBSTACLES_INITIAL = 3;
-var NUM_OF_OBSTACLES_ADD = 2;
+var NUM_OF_OBSTACLES = 10;
 var SCREEN_WIDTH = 800;
 var SCREEN_HEIGHT = 600;
 var Game = (function (_super) {
@@ -20,18 +19,21 @@ var Game = (function (_super) {
         this.randomGenerator = new random_generator_1.RandomGenerator();
         this.formatter = new formatter_1.Formatter();
         this.totalPausedTime = 0;
+        this.score = "";
     }
     Game.prototype.create = function () {
         this.world.resize(SCREEN_WIDTH, SCREEN_HEIGHT);
         this.outerSpace = this.game.add.tileSprite(0, 0, this.world.width, this.world.height, 'outerSpace');
         this.player = new player_1.Player(this.game, SCREEN_WIDTH / 2, this.world.centerY);
+        var numOfCircles = this.randomGenerator.generateRandom(NUM_OF_OBSTACLES);
         this.obstacles = this.game.add.group();
         this.obstacles.enableBody = true;
         this.game.physics.enable(this.obstacles, Phaser.Physics.ARCADE);
-        this.addObstacles(NUM_OF_OBSTACLES_INITIAL);
+        for (var i = 0; i < numOfCircles; i++) {
+            var obstacle = this.obstacles.create(this.randomGenerator.generateRandom(this.world.width), this.randomGenerator.generateRandom(this.world.height), 'circle');
+            obstacle.body.velocity.setTo(this.randomGenerator.generateRandom(VELOCITY, true) - INITIAL_OBSTACLE_SPEED, this.randomGenerator.generateRandom(VELOCITY, true));
+        }
         this.resetTime();
-        this.game.time.events.repeat(Phaser.Timer.SECOND * 10, 10, this.addObstacles, this, NUM_OF_OBSTACLES_ADD);
-        this.game.time.events.start();
     };
     Game.prototype.update = function () {
         this.game.physics.arcade.collide(this.player, this.obstacles, this.reset, null, this);
@@ -39,29 +41,18 @@ var Game = (function (_super) {
         this.outerSpace.tilePosition.x -= 3;
         this.killPlayerIfHitLeftEdge();
     };
-    Game.prototype.addObstacles = function (num) {
-        for (var i = 0; i < num; i++) {
-            var obstacle = this.obstacles.create(SCREEN_WIDTH + this.randomGenerator.generateRandom(SCREEN_WIDTH / 10), this.randomGenerator.generateRandom(SCREEN_HEIGHT), 'circle');
-            obstacle.body.velocity.setTo(this.randomGenerator.generateRandom(VELOCITY, true) - INITIAL_OBSTACLE_SPEED, this.randomGenerator.generateRandom(VELOCITY, true));
-            obstacle.checkWorldBounds = true;
-            obstacle.events.onOutOfBounds.add(this.obstacleOut, this);
-        }
-    };
-    Game.prototype.obstacleOut = function (obstacle) {
-        obstacle.reset(SCREEN_WIDTH + this.randomGenerator.generateRandom(SCREEN_WIDTH / 10), this.randomGenerator.generateRandom(SCREEN_HEIGHT));
-        obstacle.body.velocity.setTo(this.randomGenerator.generateRandom(VELOCITY, true) - INITIAL_OBSTACLE_SPEED, this.randomGenerator.generateRandom(VELOCITY, true));
-    };
     Game.prototype.killPlayerIfHitLeftEdge = function () {
         if (this.player.body.x <= this.camera.x) {
             this.reset(this.player);
         }
     };
     Game.prototype.render = function () {
-        this.game.debug.text(this.formatter.formatTime(this.getGameTime()), SCREEN_WIDTH - 80, 30, "#ffffff");
+        this.score = this.formatter.formatTime(this.getGameTime());
+        this.game.debug.text(this.score, SCREEN_WIDTH - 80, 30, "#ffffff");
     };
     Game.prototype.reset = function (player) {
         player.kill();
-        this.game.state.start('Menu');
+        this.game.state.start('Score', true, false, this.score);
     };
     Game.prototype.resetTime = function () {
         this.game.time.reset();
